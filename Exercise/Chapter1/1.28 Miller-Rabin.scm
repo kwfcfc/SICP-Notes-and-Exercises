fbadb24cda3cbbc70b403a90843a21f9535a3da6
kwfcfc <73300175+kwfcfc@users.noreply.github.com>
2021-05-27 16:10:28 +0800

    <feat> finish 1.28
    Finish 1.28 exercise. Now start the next section of Chapter 1-Formulating
    Abstractions with Higher-Order Procedures
    完成了练习1.28。现在开始第一章的最后一节，用高阶过程构造抽象。

---
+++ Exercise/Chapter1/1.28 Miller-Rabin.scm
@@ -0,0 +1,32 @@
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((and (= (remainder (square base) m) 1)
+              (not (or (= base 1)
+                       (= base (- m 1))))) 0);; The test part could be rewrite into a function.
+        ((even? exp)
+         (remainder
+          (square (expmod base (/ exp 2) m))
+          m))
+        (else
+         (remainder
+          (* base (expmod base (- exp 1) m))
+          m))))
+
+(define (miller-rabin-test n)
+  (define (try-it a)
+    (= (expmod a (- n 1) n) 1))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (miller-rabin-prime? n times)
+  (cond ((= times 0) "prime")
+        ((miller-rabin-test n) (miller-rabin-prime? n (- times 1)))
+        (else "not prime")))
+
+(miller-rabin-prime? 2 10)
+(miller-rabin-prime? 10037 10)
+(miller-rabin-prime? 100043 10)
+(miller-rabin-prime? 1009 10)
+(miller-rabin-prime? 99 10)
+(miller-rabin-prime? 4 10)
+(miller-rabin-prime? 108 10)
+(miller-rabin-prime? 10039 10)
