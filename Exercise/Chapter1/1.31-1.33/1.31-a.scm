a3102d824df051aaaaca18ea23b929cfed64df42
kwfcfc <73300175+kwfcfc@users.noreply.github.com>
2021-05-28 22:48:23 +0800

    <feat> add a new series of exercises
    Add a series of exercises about accumulation and filtering. However,
    the details about such concepts won't appear until chapter 2.
    添加了一个新的练习系列关于累积和过滤器。然而这些概念的细节不会在第二章
    之前出现。

---
+++ Exercise/Chapter1/1.31-1.33/1.31-a.scm
@@ -0,0 +1,21 @@
+(define (product term a next b)
+  (if (> a b)
+      1
+      (* (term a)
+         (product term (next a) next b))))
+
+(define (factorial n) (product (lambda(i) i) 1 (lambda(i) (+ i 1)) n))
+
+(define (pi-product a b)
+  (define (pi-term x) (/ (* (- x 1.0) (+ x 1.0)) (* x x)))
+  ;; Here try to convert the number into float.
+  (define  (pi-next x) (+ x 2))
+  (product pi-term a pi-next b))
+
+
+(factorial 5)
+(* 4 (pi-product 3 21))
+
+;; The output seems this method for calculating pi is too slow.
+;; Before it runs out of the recursion depth, the accuracy can't
+;; step further than 5 digits.
