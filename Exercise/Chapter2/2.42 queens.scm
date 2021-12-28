79a27d4d4ae73bfd04776aee23ca599314d93829
kwfcfc <73300175+kwfcfc@users.noreply.github.com>
2021-12-28 21:14:41 +0800

    <feat> better version of queens
    Change the data structure, see the comments in code.
    In my computer, the procedure will reach the maximum of recursion depth when
    the input is 12. And it is faster than the last version, which may
    even have no response at input of 11.
    改变了数据结构，请参见代码中的注释。在我的电脑上，当输入为12时，这个程
    序将会达到最大递归上限，但是这比上一个版本要快，而且上一个版本甚至当输
    入为11时就没有响应了。

--- Exercise/Chapter2/2.42 queens.scm
+++ Exercise/Chapter2/2.42 queens.scm
@@ -4,12 +4,11 @@
     (if (= k 0)
         (list empty-board)
         (filter
-         (lambda (positions) (safe? k positions))
+         (lambda (positions) (safe? positions))
          (flatmap
           (lambda (rest-of-queens)
             (map (lambda (new-row)
-                   (adjoin-position new-row k
-                                    rest-of-queens))
+                   (adjoin-position new-row rest-of-queens))
                  (enumerate-interval 1 board-size)))
           (queen-cols (- k 1))))))
   (queen-cols board-size))
@@ -17,13 +16,15 @@
 ;; the representation of a board is a list with
 ;; k elements that records the position of each queen.
 (define empty-board nil)
-(define (adjoin-position new-row k rest-of-queens)
+(define (adjoin-position new-row rest-of-queens)
   ;; the board is a list, and this procedure will
   ;; add new-row at kth place
-  ;; '(<col 1> <col 2> <col 3> <col 4>)
-  (if (= k 1)
+  ;; This is changed into '(<col 4> <col 3> <col 2> <col 1>) now
+  ;; and here it is not necessary to pass the k to procedure itself
+  ;; unless the boundary condition.
+  (if (null? rest-of-queens)
       (list new-row)
-      (accumulate cons (list new-row) rest-of-queens)))
+      (cons new-row rest-of-queens)))
 
 (define (col k positions)
   ;; find the kth queen in kth column
@@ -31,15 +32,29 @@
       (car positions)
       (col (- k 1) (cdr positions))))
        
-(define (safe? k positions)
-  (define (col-k x) (col x positions))
-  (= 0
-     (accumulate +
-                 0
-                 (map (lambda (i)
-                        (if (or (= (col-k i) (col-k k))
-                                (= (col-k i) (+ (col-k k) (- k i)))
-                                (= (col-k i) (+ (col-k k) (- i k))))
-                         1
-                         0))
-                      (enumerate-interval 1 (- k 1))))))
+(define (safe? positions)
+  (define (iter-check cnt board k)
+    (cond ((null? board) #t)
+          ((or (= k (car board))
+               (= (+ k cnt)
+                  (car board))
+               (= (- k cnt)
+                  (car board)))
+           #f)
+          (else (iter-check (+ cnt 1) (cdr board) k))))
+  ;; according to the new representation of the board, now we only need
+  ;; one argument to check the board, that is to check the (car positions)
+  ;; and the rest of the positions.
+  (iter-check 1 (cdr positions) (car positions)))
+
+(define (ordered-queens n)
+  (define (reverse l)
+    (define (iter items result)
+      (if (null? items)
+          result
+          (iter (cdr items)
+                (cons (car items) result))))
+    (iter l ()))
+  (map (lambda (x) (reverse x)) (queens n)))
+;; In my computer, the procedure will reach the maximum of recursion depth when
+;; the input is 12. And it is faster than the last version, which may even have no response at input of 11
